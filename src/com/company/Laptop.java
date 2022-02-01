24a49370550170dfe607d12adfea936fef9e3c09
Otorbaev Adilet <adilet_otor@mail.ru>
2022-02-01 19:10:52 +0600

    Laptops work

---
+++ src/com/company/Laptop.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Laptop {
+    String name;
+    String memory;
+    String weight;
+    String processor;
+    String color;
+
+    public void model() {
+        System.out.println("Ваш ноутбук "+name+" Его вес "+weight+" Цвет "+color+" Память "+memory+" Его процессор "+processor);
+
+    }
+    public void open() {
+        System.out.println("Ваш ноутбук "+name+" включен.");
+    }
+    public void close() {
+        System.out.println("Ваш ноутбук "+name+" выключен.");
+    }
+    public void lagaet(){
+        System.out.println(name+" Завис и не загружает ничего.");
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,43 @@
 package com.company;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Treugolnik uch = new Treugolnik();
-        System.out.println("Введите размеры треугольника: ");
-        uch.a = sc.nextInt();
-        uch.b = sc.nextInt();
-        uch.c = sc.nextInt();
-        System.out.print("Аянты: ");
-        uch.area(uch.a,uch.b,uch.c);
+        Laptop dellinspiron = new Laptop();
+        dellinspiron.name = "Dell Inspiron";
+        dellinspiron.color="Steel Black";
+        dellinspiron.memory="16GB-Ram";
+        dellinspiron.weight="3 kg";
+        dellinspiron.processor="AMD radeon Ryzen 3";
+        dellinspiron.model();
+        dellinspiron.open();
+        dellinspiron.close();
 
+        Laptop acer = new Laptop();
+        acer.name="Acer";
+        acer.memory="2GB-Ram";
+        acer.color="Grey";
+        acer.weight="1 kg";
+        acer.processor="Intel core 3";
+        acer.model();
+        acer.open();
+        acer.close();
+
+        Laptop samsung = new Laptop();
+        samsung.name ="Samsung";
+        samsung.processor="Intel coleron 1";
+        samsung.memory="2 gb Ram";
+        samsung.weight="5 kg";
+        samsung.color="Standart";
+        samsung.model();
+        samsung.open();
+        samsung.lagaet();
+
+        Laptop gamingAcer = new Laptop();
+        gamingAcer.name="gamingAcer";
+        gamingAcer.memory="32 GB ram";
+        gamingAcer.processor="Intel core 7";
+        gamingAcer.weight="4 kg";
+        gamingAcer.color="Red";
+        gamingAcer.open();
+        gamingAcer.close();
     }
 }
