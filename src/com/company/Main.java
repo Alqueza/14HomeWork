6775c79b68f83637297403497f7f43228c0c5797
Otorbaev Adilet <adilet_otor@mail.ru>
2022-02-04 22:01:37 +0600

    14 Home Work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,8 @@
 package com.company;
 public class Main {
     public static void main(String[] args) {
-        Curs bir = new Curs("Adilet","0706157252","Zamir","Yanvar");
-        Student student = new Student("11.11.2003","Adilet","Otorbaev",18);
-        bir.info();
-        student.info();
+        Class bir = new Class(1, "Hello world", 10);
+        bir.butun();
+        bir.all();
     }
 }
